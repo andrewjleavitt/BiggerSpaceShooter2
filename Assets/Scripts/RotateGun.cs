31897fc72daccd1e1afb09120b87c8cd1fe0538b
Andy Leavitt <andyleavitt@gmail.com>
2017-03-10 23:18:27 -0700

    Initial Commit

---
+++ Assets/Scripts/RotateGun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGun : MonoBehaviour {
+
+	void Update () {
+		Rigidbody2D enemyRb2d = FindClosestEnemy().GetComponent<Rigidbody2D> ();
+		Vector3 dir = enemyRb2d.transform.position - transform.position;
+		float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
+	GameObject FindClosestEnemy () {
+		GameObject[] enemies;
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = transform.position;
+		foreach (GameObject enemy in enemies) {
+			Vector3 diff = enemy.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if (curDistance < distance) {
+				closest = enemy;
+				distance = curDistance;
+			}
+		}
+		return closest;
+	}
+}
---
+++ Assets/Scripts/ShootGun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootGun : MonoBehaviour {
+
+	public GameObject shot;
+	public float fireRate;
+	public Transform shotSpawn;
+
+	private float nextFire;
+
+	void Update () {
+		if(Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			GameObject clone;
+			Quaternion rotationAmount = Quaternion.Euler(0, 0, -90);
+			clone = Instantiate(shot, shotSpawn.position, shotSpawn.rotation*rotationAmount) as GameObject;
+		}
+	}
+}
